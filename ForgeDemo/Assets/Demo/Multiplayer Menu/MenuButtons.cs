bad33640706f19628bb120a71d4b6694b9be6137
Ryan Tan <ghost_heartbeat@yahoo.com.sg>
2020-01-18 12:20:28 +0800

    Unizpped and updated to Unity version 2019.3.0a11

---
+++ ForgeDemo/Assets/Demo/Multiplayer Menu/MenuButtons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButtons : MonoBehaviour
+{
+    //These are just references to gameobjects in the multiplayer menu scene.
+    public GameObject HostBtn, JoinBtn, IPINput, PortInput;
+
+    void Start() {
+        GetButtons();
+    }
+
+    //This simply gets the multiplayer menu script set back up with its references as it will lose all of them when it is moved to the Online scene.
+    public void GetButtons() {
+        HostBtn.GetComponent<Button>().onClick.RemoveAllListeners();
+        JoinBtn.GetComponent<Button>().onClick.RemoveAllListeners();
+
+        HostBtn.GetComponent<Button>().onClick.AddListener(() => { MultiplayerMenu.Instance.Host(); });
+        JoinBtn.GetComponent<Button>().onClick.AddListener(() => { MultiplayerMenu.Instance.Connect(); });
+        MultiplayerMenu.Instance.ipAddress = IPINput.GetComponent<InputField>();
+        MultiplayerMenu.Instance.portNumber = PortInput.GetComponent<InputField>();
+    }
+}
---
+++ ForgeDemo/Assets/Demo/Online Scene/OnlineSceneMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlineSceneMenu : MonoBehaviour
+{
+    // This is the funtion attached to the OnClick listner of the Disconnect Button in the Online Scene.    
+    public void DisconnectBTN() {
+        // This calls the Disconnect From Server Void from the Multiplayer menu script that has lived from the first scene to the online scene.
+        MultiplayerMenu.Instance.DisconnectFromServer(); 
+    }
+}
